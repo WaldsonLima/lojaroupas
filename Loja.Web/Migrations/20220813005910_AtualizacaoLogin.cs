efb55b1a9cc5d2164c90bc7b54f94b3b20641409
Waldson Lima <waldsonh@gmail.com>
2022-08-12 22:07:45 -0300

    cirando readme

---
+++ Loja.Web/Migrations/20220813005910_AtualizacaoLogin.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Loja.Web.Migrations
+{
+    public partial class AtualizacaoLogin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
